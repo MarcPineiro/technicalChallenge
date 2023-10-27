4e7bb677a05f4ff6344318c639684e0a388ac4da
Marc <marc.pineiro.selas@outlook.es>
2023-10-27 18:33:50 +0200

    added sql file to configure database to be able to test the code

---
+++ setup_mysql.sql
@@ -0,0 +1,8 @@
+DROP DATABASE IF EXISTS `mysqldb`;
+CREATE DATABASE `mysqldb`
+    DEFAULT CHARACTER SET utf8
+    DEFAULT COLLATE utf8_general_ci;
+
+USE mysql;
+GRANT ALL PRIVILEGES ON *.* TO 'mysql-user'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
