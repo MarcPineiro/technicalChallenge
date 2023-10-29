a159372011030391bfb322f5e9d3d4623367820f
MarcPineiro <149034003+MarcPineiro@users.noreply.github.com>
2023-10-29 18:59:15 +0100

    Create user added

--- setup_mysql.sql
+++ setup_mysql.sql
@@ -2,7 +2,7 @@ DROP DATABASE IF EXISTS `mysqldb`;
 CREATE DATABASE `mysqldb`
     DEFAULT CHARACTER SET utf8
     DEFAULT COLLATE utf8_general_ci;
-
+CREATE USER 'mysql-user'@'localhost' IDENTIFIED BY 'password';
 USE mysql;
 GRANT ALL PRIVILEGES ON *.* TO 'mysql-user'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
