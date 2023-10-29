2f165aab46fa23e4fdb513475fc2aeec5bcd07a5
MarcPineiro <149034003+MarcPineiro@users.noreply.github.com>
2023-10-29 19:35:55 +0100

    Removed drop line added by error

--- setup_mysql.sql
+++ setup_mysql.sql
@@ -2,7 +2,5 @@ DROP DATABASE IF EXISTS `mysqldb`;
 CREATE DATABASE `mysqldb`
     DEFAULT CHARACTER SET utf8
     DEFAULT COLLATE utf8_general_ci;
-drop user admin@localhost;
-
 GRANT ALL PRIVILEGES ON *.* TO 'mysql-user'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
