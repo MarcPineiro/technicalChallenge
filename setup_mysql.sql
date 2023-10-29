cae671cd09ce3c3460edca29a2ad190bc7e2db71
MarcPineiro <149034003+MarcPineiro@users.noreply.github.com>
2023-10-29 19:34:07 +0100

    Changed host of grant

--- setup_mysql.sql
+++ setup_mysql.sql
@@ -2,5 +2,7 @@ DROP DATABASE IF EXISTS `mysqldb`;
 CREATE DATABASE `mysqldb`
     DEFAULT CHARACTER SET utf8
     DEFAULT COLLATE utf8_general_ci;
-GRANT ALL PRIVILEGES ON *.* TO 'mysql-user'@'$' WITH GRANT OPTION;
+drop user admin@localhost;
+
+GRANT ALL PRIVILEGES ON *.* TO 'mysql-user'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
