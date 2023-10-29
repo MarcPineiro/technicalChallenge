13d342d602ae461b7a0dd468b56db16397624268
MarcPineiro <149034003+MarcPineiro@users.noreply.github.com>
2023-10-29 19:27:21 +0100

    Changed host sql

--- setup_mysql.sql
+++ setup_mysql.sql
@@ -2,7 +2,6 @@ DROP DATABASE IF EXISTS `mysqldb`;
 CREATE DATABASE `mysqldb`
     DEFAULT CHARACTER SET utf8
     DEFAULT COLLATE utf8_general_ci;
-CREATE USER 'mysql-user'@'localhost' IDENTIFIED BY 'password';
-USE mysql;
-GRANT ALL PRIVILEGES ON *.* TO 'mysql-user'@'localhost' WITH GRANT OPTION;
+CREATE USER 'mysql-user'@'%' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON *.* TO 'mysql-user'@'$' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
